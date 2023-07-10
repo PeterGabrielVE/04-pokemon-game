b45267a7f73e5bdb84642312d6be5bc9ec22a61a
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-07-10 19:35:08 -0400

    estructura del proyecto y componentes

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>¿Quien es este Pokemon?</h1>
+
+  <PokemonPicture />
+  <PokemonOptions />
+
+</template>
+
+<script>
+import PokemonPicture from '@/components/PokemonPicture.vue'
+import PokemonOptions from '@/components/PokemonOptions.vue'
+
+export default {
+    components: {
+        PokemonPicture,
+        PokemonOptions
+    }
+}
+</script>
+
+<style>
+
+</style>
