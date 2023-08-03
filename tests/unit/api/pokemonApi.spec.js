decb4629e7fbb8e8b880c1a2182e4741d91d7bd6
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-08-03 15:37:50 -0400

    test unit

--- tests/unit/api/pokemonApi.spec.js
+++ tests/unit/api/pokemonApi.spec.js
@@ -1,11 +1,13 @@
 import pokemonApi from '@/api/pokemonApi'
 
-describe('pokemonApi', () =>{
-   
-    test('axios debe de estar configurado en el api de pokemon', () =>{
-
+describe('pokemonApi', () => {
+    
+    test('axios debe de estar configurado con el api de pokemon', () => {
+        
         expect( pokemonApi.defaults.baseURL )
-              .toBe('https://pokeapi.co/api/v2/pokemon')
+            .toBe('https://pokeapi.co/api/v2/pokemon')
 
     })
+    
+
 })
